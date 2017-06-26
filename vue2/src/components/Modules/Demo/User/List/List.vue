6fa73823185e3ce65447ae7fa21faaf79fb1fc4c
卢海涛 <1006453754@qq.com>
2017-06-26 10:10:35 +0800

    mock  管理

---
+++ vue2/src/components/Modules/Demo/User/List/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="list">
+         <el-button>添加项目</el-button>  
+    </div>
+</template>
+
+<script>
+	import ListJs from './List.js';
+	module.exports = ListJs;
+</script>
+<style scoped lang='less'>
+    .demo-form-inline {
+        display: inline-block;
+        float: right;
+    }
+
+    .btm-action {
+        margin-top: 20px;
+        text-align: center;
+    }
+
+    .actions-top {
+        height: 46px;
+    }
+
+    .pagination {
+        display: inline-block;
+    }
+</style>
